57ad4c0b331da16d44dccdf6d16e670e49df08e9
matetipping <matetipping@gmail.com>
2017-01-24 10:18:43 +1100

    Update current_2.js

--- current_2.js
+++ current_2.js
@@ -1,11 +1,11 @@
 var tipping_data = {
     round: "R1",
     admin: "Administrator",
-	tips: [
-	    ["R1", "Administrator", "CAR", 32, "HAW", 25, 0, 0],
-	    ["R1", "Daniel Terrington", "RIC", 3, "HAW", 34, 0, 0],
-	    ["R1", "ciniboi_12", "CAR", 10, "HAW", 19, 0, 0]
-	],
+    tips: [
+	["R1", "Administrator", "CAR", 32, "HAW", 25, 0, 0],
+	["R1", "Daniel Terrington", "RIC", 3, "HAW", 34, 0, 0],
+	["R1", "ciniboi_12", "CAR", 10, "HAW", 19, 0, 0]
+    ],
     fixtures: [
         ["R1", "Administrator", "Daniel Terrington"],
         ["R2"],
@@ -15,5 +15,5 @@ var tipping_data = {
 
 $(function () {
     $("span#score_user").html(tipping_data.admin);
-    $("span#score_opponent").html(String.valueOf(tipping_data.tips[3]));
+    $("span#score_opponent").html(String.valueOf(tipping_data.tips[0][3]));
 });
