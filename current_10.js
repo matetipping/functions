6cea25af2196e4499822cf5327b21536f37bb717
matetipping <matetipping@gmail.com>
2017-01-24 10:35:10 +1100

    Update and rename current_9.js to current_10.js

---
+++ current_10.js
@@ -0,0 +1,23 @@
+var tipping_data = {
+    round: "R1",
+    admin: "Administrator",
+    tips: [
+	["R1", "Administrator", "CAR", 32, "HAW", 25, 0, 0],
+	["R1", "Daniel Terrington", "RIC", 3, "HAW", 34, 0, 0],
+	["R1", "ciniboi_12", "CAR", 10, "HAW", 19, 0, 0]
+    ],
+    fixtures: [
+        ["R1", "Administrator", "Daniel Terrington"],
+        ["R2"],
+        ["R3"]
+    ]
+}
+
+function find_round_tips(round, a_list) {
+    return a_list.length;
+}
+
+$(function () {
+    $("span#score_user").html(find_round_tips(tipping_data.round, tipping_data.tips));
+    $("span#score_opponent").html("hi");
+});
