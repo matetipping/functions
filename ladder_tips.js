ccb15ba856fae15197cef930becf0eb15b510b32
matetipping <matetipping@gmail.com>
2017-01-27 22:07:05 +1100

    Create ladder_tips.js

---
+++ ladder_tips.js
@@ -0,0 +1,22 @@
+var ladder_data = {
+    tips: [
+        ["Administrator", "GWS", "WBD", "SYD", "ADE", "GEE", "HAW", "MEL", "STK", "WCE", "CAR", "PTA", "ESS", "COL", "RIC", "GCS", "NTH", "FRE", "BRI"],
+        []
+    ]
+}
+
+function set_image(id, value) {
+    low_val = value.toLowerCase();
+    $("span#ladderimg_" + id + " div.team_block").attr("id", low_value);
+}
+
+function remove_team(value) {
+    low_val = value.toLowerCase();
+    var i;
+    for (i = 0; i < 18; i++) {
+         var img_id = $("span#ladderimg_" + i + " div.team_block").attr("id");
+         if (img_id === low_val) {
+             $("span#ladderimg_" + i + " div.team_block").removeAttr("id");
+         }
+    }
+}
