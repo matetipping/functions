596b8928cd1f55f9b5ebe7c5e6e748214103df14
matetipping <matetipping@gmail.com>
2017-01-27 11:42:48 +1100

    Update and rename afl_records_8.js to afl_records_9.js

--- afl_records_9.js
+++ afl_records_9.js
@@ -25,8 +25,7 @@ function set_match(i, match_dets) {
     var team_a = match_dets[1];
     var team_b = match_dets[2];
     var venue = match_dets[3];
-    var num = i.toString();
-    $("tipping_matchname_" + num).html(team_a + " vs " + team_b);
+    $("h3#tipping_matchname_" + i).html(team_a + " vs " + team_b);
 }
 
 function set_all_matches(round_no) {
