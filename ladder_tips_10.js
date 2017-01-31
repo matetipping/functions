c9732c0c088fea9c29c0c08b8922482991c454cc
matetipping <matetipping@gmail.com>
2017-01-31 21:52:38 +1100

    Update and rename ladder_tips_9.js to ladder_tips_10.js

--- ladder_tips_10.js
+++ ladder_tips_10.js
@@ -24,6 +24,17 @@ function swap_teams(idA, idB) {
     $("select#ladder_team_" + idB).val(tempA.toUpperCase());
 }
 
+function show_remaining() {
+    var i;
+    $("td.remaining_teams div.team_block").each() {
+        $(this).show();
+    };
+    for (i = 0; i < 18; i++) {
+        var img_id = $("span#ladderimg_" + i + " div.team_block").attr("id");
+        $("td.remaining_teams div#" + img_id + ".team_block").hide();
+    }
+}
+
 function set_image(id, value) {
     var low_val = value.toLowerCase();
     $("span#ladderimg_" + id + " div.team_block").attr("id", low_val);
