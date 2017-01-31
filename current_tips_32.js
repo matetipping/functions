fc74cf753cdb593a9c7362c208400b85585aa5cb
matetipping <matetipping@gmail.com>
2017-01-31 13:06:16 +1100

    Update current_tips_32.js

--- current_tips_32.js
+++ current_tips_32.js
@@ -244,13 +244,13 @@ function set_names(player_name, opponent_name) {
 
 $(function () {
     var me = get_this_player();
+    var them = get_opponent(me, tipping_data.round, tipping_data.fixtures);
+    set_scores(me, them);
+    set_names(me, them);
     var i;
     var round_fix = find_round_tips(tipping_data.round, tipping_data.fixtures)[0];
     var len = round_fix.length;
     var player_check = "";
-    var them = get_opponent(me, tipping_data.round, tipping_data.fixtures);
-    set_scores(me, them);
-    set_names(me, them);
     for (i = 0; i < len; i++) {
         player_check = round_fix[i];
 	if (player_check === me) {
