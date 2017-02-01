8da5948596be0e3d6813fbf8e6226574c3c5c4e5
matetipping <matetipping@gmail.com>
2017-02-01 23:08:53 +1100

    Create mate_ladder.js

---
+++ mate_ladder.js
@@ -0,0 +1,16 @@
+var ladder = [];
+var round_no = tipping_data.round;
+var results = find_player_tips(tipping_data.admin, tipping_data.tips);
+var tips = tipping_data.tips;
+var round_one_tips = find_round_tips("R1", tips) {
+
+var i;
+var len = round_one_tips.length;
+
+for (i = 1; i < len; i++) {
+    ladder.push([round_one_tips[i][1]]);
+}
+
+for (i = 0; i < len; i++) {
+    var 
+}
