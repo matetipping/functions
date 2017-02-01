71ee48a671b566e9ab99b9a99d5ccd6b9c92fdc5
matetipping <matetipping@gmail.com>
2017-02-01 16:52:28 +1100

    Update current_tips_32.js

--- current_tips_32.js
+++ current_tips_32.js
@@ -12,7 +12,26 @@ var tipping_data = {
     fixtures: [
         ["R1", "ciniboi_12", "Daniel Terrington", "Administrator", "Jono22", "Steve", "Craig Terrington"],
         ["R2"],
-        ["R3"]
+        ["R3"],
+	["R4"],
+	["R5"],
+	["R6"],
+	["R7"],
+        ["R8"],
+        ["R9"],
+	["R10"],
+	["B1"],
+	["B2"],
+	["R14"],
+        ["R15"],
+        ["R16"],
+	["R17"],
+	["R18"],
+	["R19"],
+	["R20"],
+        ["R21"],
+        ["R22"],
+	["R23"]
     ]
 }
 
