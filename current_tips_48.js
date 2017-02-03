0ded7ea7f8ec2365a71edbe46b72980c00b81be8
matetipping <matetipping@gmail.com>
2017-02-04 00:04:05 +1100

    Update and rename current_tips_47.js to current_tips_48.js

--- current_tips_48.js
+++ current_tips_48.js
@@ -67,12 +67,12 @@ function set_bonus_remaining(player_name) {
     var bonuses = get_bonus_tip_count(player_name);
     var count_disp = bonuses[0];
     var count_scor = bonuses[1];
-    $("h4#tipping_remaining_disp").html(count_disp + "of 10 used:");
-    $("h4#tipping_remaining_scor").html(count_scor + "of 10 used:");
-    if (rem_disp <= 0) {
+    $("h4#tipping_remaining_disp").html(count_disp + "/10 used:");
+    $("h4#tipping_remaining_scor").html(count_scor + "/10 used:");
+    if (count_disp >= 10) {
         $("input#tipping_player_disposals").parents('tr').hide();
     }
-    if (rem_scor <= 0) {
+    if (count_scor >= 10) {
 	$("input#tipping_player_scorer").parents('tr').hide();
     }
 }
