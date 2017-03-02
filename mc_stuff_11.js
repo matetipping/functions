ad7958e4d884cf801fc9e7aca60989fd75d7fb9a
matetipping <matetipping@gmail.com>
2017-03-02 12:26:58 +1100

    Update and rename mc_stuff_10.js to mc_stuff_11.js

--- mc_stuff_11.js
+++ mc_stuff_11.js
@@ -1,17 +1,17 @@
 function change_career_options() {
     var profession = $("select#attr_prof").val();
     if (profession === "0") {
-            $("select#attr_career").html("<option value='0'>None</option><option value='1'>Farmer</option><option value='2'>Fisherman</option><option value='3'>Shepherd</option><option value='4'>Fletcher</option>");
+            $("select#attr_career").html("<option value='0'>Random</option><option value='1'>Farmer</option><option value='2'>Fisherman</option><option value='3'>Shepherd</option><option value='4'>Fletcher</option>");
     } else if (profession === "1") {
-            $("select#attr_career").html("<option value='0'>None</option><option value='1'>Librarian</option><option value='2'>Cartographer</option>");
+            $("select#attr_career").html("<option value='0'>Random</option><option value='1'>Librarian</option><option value='2'>Cartographer</option>");
     } else if (profession === "2") {
-            $("select#attr_career").html("<option value='0'>None</option><option value='1'>Cleric</option>");
+            $("select#attr_career").html("<option value='0'>Random</option><option value='1'>Cleric</option>");
     } else if (profession === "3") {
-            $("select#attr_career").html("<option value='0'>None</option><option value='1'>Armorer</option><option value='2'>Weaponsmith</option><option value='3'>Tool Smith</option>");
+            $("select#attr_career").html("<option value='0'>Random</option><option value='1'>Armorer</option><option value='2'>Weaponsmith</option><option value='3'>Tool Smith</option>");
     } else if (profession === "4") {
-            $("select#attr_career").html("<option value='0'>None</option><option value='1'>Butcher</option><option value='2'>Leatherworker</option>");
+            $("select#attr_career").html("<option value='0'>Random</option><option value='1'>Butcher</option><option value='2'>Leatherworker</option>");
     } else {
-            $("select#attr_career").html("<option value='0'>None</option>");
+            $("select#attr_career").html("<option value='0'>Random</option>");
     }
 }
 
