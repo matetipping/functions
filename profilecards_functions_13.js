8d8a8e81581fb258108572d0b283700b46d24e72
matetipping <matetipping@gmail.com>
2017-04-03 22:10:29 +1000

    Update and rename profilecards_functions_12.js to profilecards_functions_13.js

--- profilecards_functions_13.js
+++ profilecards_functions_13.js
@@ -10,6 +10,7 @@ var awards = {
                 awards.present(a);
             }
         }
+	awards.marquee(a);
     },
     present: function(a) {
         var award = t_award.users[a];
@@ -20,6 +21,16 @@ var awards = {
         } else {
             $("." + award[0] + "-awards").append('<img onmouseover="awards.tooltip.open(event,' + a + ');" onmouseout="awards.tooltip.bye(' + a + ');" id="' + a + '-award" src="' + award[2] + '" alt="' + award[1] + '" width="' + t_award.thumbnail[0] + 'px" height="' + t_award.thumbnail[1] + 'px" />');
         }
+    },
+    marquee: function(a) {
+        var award = t_award.users[a];
+	if (award[0] === $("div#top strong a").attr("href").split("/")[4]) {
+	    var current = $("marquee#card_marquee").html()
+	    $("marquee#card_marquee").html(current + '<img onmouseover="awards.tooltip.open(event,' + a + ');" onmouseout="awards.tooltip.bye(' + a + ');" id="' + a + '-award" src="' + award[2] + '" alt="' + award[1] + '" width="' + t_award.thumbnail[0] + 'px" height="' + t_award.thumbnail[1] + 'px" />');
+	}
+	if ($("marquee#card_marquee").html() === "") {
+	    $("marquee#card_marquee").remove();
+	}
     },
     tooltip: {
         current: 0,
