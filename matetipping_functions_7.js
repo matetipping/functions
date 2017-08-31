77c564d4d218f91aa75cd84751fc54048ffa4015
matetipping <matetipping@gmail.com>
2017-08-31 20:40:32 +1000

    Update and rename matetipping_functions_6.js to matetipping_functions_7.js

--- matetipping_functions_7.js
+++ matetipping_functions_7.js
@@ -19,7 +19,6 @@ function get_ladder_tips(round_no, name) {
     for (i = 0; i < len; i++) {
         if (afl_data.fixtures[i][0] === tipping_data.round) {
 	    number_rounds ++;
-	    alert(number_rounds);
 	}
     }
     
@@ -232,6 +231,7 @@ function calculate_scores(player_name, opponent, results_name, round_no, tips_fu
     var scale_fac = 1;
     for (i = 1; i <= round_length; i++) {
         if (round_no === "F1") {
+		alert("test");
 		if ((i == 1) || (i == 3) || (i == 5) || (i == 7)) {
 			scale_fac = 0.75;
 		} else if ((i == 2) || (i == 4) || (i == 6) || (i == 8)) {
