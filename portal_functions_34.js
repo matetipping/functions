f51c77b73ff9ca37006697f6b30ef8d2d99ad9f0
matetipping <matetipping@gmail.com>
2018-02-26 18:48:25 +1100

    Update and rename portal_functions_33.js to portal_functions_34.js

--- portal_functions_34.js
+++ portal_functions_34.js
@@ -158,13 +158,13 @@ function set_bonus_remaining(player_name) {
         var bonuses = get_bonus_tip_count(player_name);
         var count_disp = bonuses[0];
         var count_scor = bonuses[1];
-        $("h4#tipping_remaining_disp").html(count_disp + "/10 used:");
-        $("h4#tipping_remaining_scor").html(count_scor + "/10 used:");
+        $("h4#tipping_remaining_disp").html(count_disp + "/8 used:");
+        $("h4#tipping_remaining_scor").html(count_scor + "/8 used:");
         $("tr.homeaway").remove();
-        if (count_disp >= 10) {
+        if (count_disp >= 8) {
             $("input#tipping_player_disposals").parents('tr').hide();
         }
-        if (count_scor >= 10) {
+        if (count_scor >= 8) {
             $("input#tipping_player_scorer").parents('tr').hide();
         }
     }
